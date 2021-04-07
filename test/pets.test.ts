e5e0f36ad553f71eac21c33544a24f5c9d60bec6
Ryagin Yurij <y.ryagin@xrm.ru>
2021-04-07 20:51:59 +0500

    second test + refactoring

--- test/pets.test.ts
+++ test/pets.test.ts
@@ -1,14 +1,23 @@
 import got from "got"
+import { URLSearchParams } from 'url'
 import {strict as assert} from 'assert'
+import { PetController} from '../api/controller/pet.controller'
 
 describe('Pet', function()  
 {
-    it('cat be recived by id', async function()
+    it('pet be recived by id', async function()
     {
         const id = 3
-        const response  = await got (`http://93.126.97.71:10080/api/pet/${id}`)
-        const body = JSON.parse(response.body)
+        const pet =  await new PetController().getById(id)
         
-        assert(id == body.id, `Expected returned pet id to be ${id}`) 
+        assert(id == pet.id, `Expected returned pet id to be ${id}`) 
+    })
+
+    it('pet be recived by tag', async function()
+    {
+        const pets = await new PetController().findByTags('tag1')
+        
+        assert(pets.length > 0)
+        assert(pets.every(pet => pet.tags.some(tag => tag.name == 'tag1')))
     })
 })
